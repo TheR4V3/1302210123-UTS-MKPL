29b64865dd7911effe56b045fe1c60d99b2428b6
Muhammad Rafa Eka Pramoedya <91171858+TheR4V3@users.noreply.github.com>
2024-04-24 01:54:46 +0700

    Memperbaiki long parameter di getAnnualIncomeTax
    Memperbaiki long parameter di getAnnualIncomeTax dengan membuat class baru untu menyimpan pajak

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -46,6 +46,17 @@ public class Employee {
 		
 		children = new LinkedList<>();
 	}
+	public TaxInfo getTaxInfo() {
+    TaxInfo taxInfo = new TaxInfo();
+    taxInfo.setMonthlySalary(monthlySalary);
+    taxInfo.setOtherMonthlyIncome(otherMonthlyIncome);
+    taxInfo.setMonthWorkingInYear(monthWorkingInYear);
+    taxInfo.setAnnualDeductible(annualDeductible);
+    taxInfo.setHasSpouse(!spouseIdNumber.equals(""));
+    taxInfo.setNumberOfChildren(childIdNumbers.size());
+    return taxInfo;
+}
+
 	
 	public void setMonthlySalary(int grade) {	
 		if (grade == 1) {
@@ -85,15 +96,60 @@ public class Employee {
 	}
 	
 	public int getAnnualIncomeTax() {
-		
 		LocalDate date = LocalDate.now();
-		
 		if (date.getYear() == yearJoined) {
 			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
+		} else {
 			monthWorkingInYear = 12;
 		}
-		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
+		return TaxFunction.calculateTax(getTaxInfo());
+	}	
+}
+
+public class TaxInfo {
+    private int monthlySalary;
+    private int otherMonthlyIncome;
+    private int monthWorkingInYear;
+    private int annualDeductible;
+    private boolean hasSpouse;
+    private int numberOfChildren;
+
+	public int getMonthlySalary() {
+		return monthlySalary;
 	}
+	public void setMonthlySalary(int monthlySalary) {
+		this.monthlySalary = monthlySalary;
+	}
+	public int getOtherMonthlyIncome() {
+		return otherMonthlyIncome;
+	}
+	public void setOtherMonthlyIncome(int otherMonthlyIncome) {
+		this.otherMonthlyIncome = otherMonthlyIncome;
+	}
+	public int getMonthWorkingInYear() {
+		return monthWorkingInYear;
+	}
+	public void setMonthWorkingInYear(int monthWorkingInYear) {
+		this.monthWorkingInYear = monthWorkingInYear;
+	}
+	public int getAnnualDeductible() {
+		return annualDeductible;
+	}
+	public void setAnnualDeductible(int annualDeductible) {
+		this.annualDeductible = annualDeductible;
+	}
+	public boolean isHasSpouse() {
+		return hasSpouse;
+	}
+	public void setHasSpouse(boolean hasSpouse) {
+		this.hasSpouse = hasSpouse;
+	}
+	public int getNumberOfChildren() {
+		return numberOfChildren;
+	}
+	public void setNumberOfChildren(int numberOfChildren) {
+		this.numberOfChildren = numberOfChildren;
+	}
+
+    
 }
