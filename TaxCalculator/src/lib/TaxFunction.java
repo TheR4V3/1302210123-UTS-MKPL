fc58a3c892eaefff15d715f40daa22b7c7750e23
Muhammad Rafa Eka Pramoedya <91171858+TheR4V3@users.noreply.github.com>
2024-04-24 02:19:19 +0700

    Mengubah Variabel Taxfunction
    Mengubah boolean menjadi enum

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -1,8 +1,13 @@
 package lib;
 
+
+
 public class TaxFunction {
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
+	
+	public int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, MaritalStatus maritalStatus, int numberOfChildren) {
+		public enum MaritalStatus {
+    		MARRIED, UNMARRIED
+		}	
 		int tax = 0;
 		
 		if (numberOfMonthWorking > 12) {
@@ -13,7 +18,7 @@ public class TaxFunction {
 			numberOfChildren = 3;
 		}
 		
-		if (isMarried) {
+		if (maritalStatus == MaritalStatus.MARRIED) {
 			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
 		}else {
 			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
